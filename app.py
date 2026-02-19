831aa793ae548e9a9a7fcac520517f3a7637f4cc
Isaac <maraniisaac1541@gmail.com>
2026-02-19 10:27:27 +0300

    sokogarden api first commit

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import *
+import pymysql
+
+app=Flask(__name__)
+
+@app.route("/api/signup", methods=["POST"])
+def signup():
+    username = request.form["username"]
+    email = request.form["email"]
+    phone = request.form["phone"]
+    password = request.form["password"]
+
+    print(username,email, phone, password)
+    # create connection
+    connection = pymysql.connect(host="localhost", user="root", password="", database="marani_sokogarden")
+
+    # create cursor to handle sql queries
+    cursor = connection.cursor()
+
+    # create the sql query
+
+    sql="insert into users (username, email, phone, password) values (%s, %s, %s, %s)"
+    
+    # data to be saved
+    data = (username, email, phone, password)
+    print(data)
+
+    # execute the sql query
+    cursor.execute(sql, data)
+    # save the data 
+    connection.commit()
+    
+    # return response
+    return jsonify({"mesage": "Sign up successfull"})
+
+@app.route("/api/login", methods=["POST"])
+def login():
+    email=request.form["email"]
+    password=request.form["password"]
+
+    print(email, password)
+
+    # create connection
+    connection = pymysql.connect(host="localhost", user="root", password="", database="marani_sokogarden")
+
+    #create cursor to handle sql queries
+    # cursor = connection.cursor()
+    
+    # cursor to fetch data as key-value pair
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+    # create the sql query
+    sql="select user_id, username, email, phone from users where email = %s and password = %s"
+
+    # data to execute the sql query
+    data = (email, password)
+
+    # execute the sql query
+    cursor.execute(sql, data)
+
+    # data to execute the sql query
+    data=(email, password)
+
+    # check resulting rows
+    if cursor.rowcount == 0:
+        return jsonify({"message": "invalid credential"})
+    else:
+        # get the user data
+        user=cursor .fetchone()
+        return jsonify({"message": "login successfully", "user": user})
+    return jsonify({"message": "loged in successfully"})                                   
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)   
